ce68f8603116784467368189f1a07135246f1e19
atsumaru1377 <atsumaru1377@gmail.com>
2023-07-26 19:02:45 +0900

    update camera module

--- Components/WebcamCapture.jsx
+++ Components/WebcamCapture.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useRef } from "react";
+// Components/WebcamCapture.js
+import React, { useEffect, useRef } from 'react';
 
-const Capture = () => {
+const WebcamCapture = ({ onCapture }) => {
   const videoRef = useRef();
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({
-        video: { facingMode: "environment" }, // 外側のカメラを使用
-        audio: false, // 音声は不要なので false
+        video: { facingMode: 'environment' },
+        audio: false
       })
       .then((stream) => {
         let video = videoRef.current;
@@ -17,9 +18,24 @@ const Capture = () => {
       .catch((err) => {
         console.log("An error occurred! " + err);
       });
+
+    const capture = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      const context = canvas.getContext('2d');
+      context.drawImage(videoRef.current, 0, 0);
+      onCapture(canvas.toDataURL('image/png'));
+    };
+
+    const intervalId = setInterval(capture, 1000); // every second
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return <video ref={videoRef} />;
 };
 
-export default Capture;
+export default WebcamCapture;
--- pages/books.js
+++ pages/books.js
@@ -1,46 +1,43 @@
 // pages/books.js
-import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-import Quagga from "quagga";
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import Quagga from 'quagga';
 
 const WebcamCapture = dynamic(
-  () => import("../Components/WebcamCapture"),
+  () => import('../components/WebcamCapture'),
   { ssr: false }
 );
 
 const Books = () => {
   const [isbn, setIsbn] = useState(null);
-  const [windowWidth, setWindowWidth] = useState(0);
 
   const handleCapture = (imageSrc) => {
     Quagga.decodeSingle({
       decoder: {
-        readers: ["ean_reader"] // this reader is for ISBN
+        readers: ['ean_reader']
       },
       locate: true,
       src: imageSrc,
-      numOfWorkers: 0, // Needs to be 0 when used within node
+      numOfWorkers: 0,
       inputStream: {
-        size: 800,
-      },
-    }, function (result) {
-      if (result.codeResult) {
+        size: 800
+      }
+    }, (result) => {
+      if (result && result.codeResult) {
         setIsbn(result.codeResult.code);
       } else {
-        setIsbn("No barcode detected");
+        console.log('No barcode detected');
       }
     });
   };
-
-  useEffect(() => {
-    setWindowWidth(window.innerWidth);
-  }, []);
 
   useEffect(() => {
     const fetchBookData = async () => {
       if (!(isbn && isbn.length === 13)) return;
       try {
         const res = await fetch(`/api/get_book_data?isbn=${isbn}`);
+        const data = await res.json();
+        console.log(data);
       } catch (error) {
         console.error(`Error: ${error}`);
       }
@@ -51,7 +48,7 @@ const Books = () => {
 
   return (
     <div>
-      {<WebcamCapture onCapture={handleCapture} />}
+      <WebcamCapture onCapture={handleCapture} />
       {isbn && <div>ISBN: {isbn}</div>}
     </div>
   );
